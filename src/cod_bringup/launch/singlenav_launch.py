84e2a7e0b19452e50b510afb2a5e6e6a3f5f6aea
WAN-37 <tomwjunyu1216@gmail.com>
2026-05-02 03:59:47 +0000

    single_nav 已经通  ， 后面初始位姿（map -> odom）还要优化

--- src/cod_bringup/launch/singlenav_launch.py
+++ src/cod_bringup/launch/singlenav_launch.py
@@ -27,7 +27,7 @@ def generate_launch_description():
         'nav2_params_file',default_value=os.path.join(bring_up_dir,'params','singlenav2_params.yaml')
     )
     declare_map_yaml_file = DeclareLaunchArgument(
-        'map', default_value=os.path.join(bring_up_dir, 'maps', 'auto_save', 'auto_map_113440.yaml'),
+        'map', default_value=os.path.join(bring_up_dir, 'maps', 'auto_save', 'auto_map_122020.yaml'),
         description='Full path to map yaml file to load')
     declare_use_realsense = DeclareLaunchArgument(
         'use_realsense', default_value='false',
@@ -104,7 +104,7 @@ def generate_launch_description():
                     "--pitch",
                     "0.0",
                     "--yaw",
-                    "-0.5",
+                    "0.0",
                     "--frame-id",
                     "map",
                     "--child-frame-id",
